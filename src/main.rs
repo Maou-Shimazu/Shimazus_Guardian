f0e092de05163ad0cadb65e71cdd01b114089d61
Eldad Danladi <eldaddanladi@gmail.com>
2022-08-20 02:38:29 -0600

    Join dm

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,11 @@ impl EventHandler for Handler {
                     let _ = member.add_role(&ctx.http, 785220867180724245).await;
                     let _ = member.add_role(&ctx.http, 714157786824441886).await;
                     let _ = member.remove_role(&ctx.http, 853098704063037460).await;
+                    let _ = command.clone().user.direct_message(&ctx, |message| {
+                        message.content(
+                            "Grimgar: Remastered (In-Dev):\n\nThank you for verifying! Don't forget to read rules also!",
+                        )
+                    }).await;
                     "Verified!".into()
                 }
                 _ => "Unimplimented".into(),
